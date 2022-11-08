7aec20468f06f810b15c6b3cc8268aacd991ff64
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-11-08 12:36:52 +0300

    week09: ex1 seems to work better

--- week09/ex1.c
+++ week09/ex1.c
@@ -7,35 +7,28 @@
 
 typedef struct VirtualPage{
     int number;
-    char* counter_bits_string;
+    char counter_bits_string[AGING_BITS];
     int counter;
     int r_bit;
     int is_present;
 } t_data;
 
-char* revstr(char *s) {
-    int len = strlen(s);
-    char* tmp = malloc(sizeof(char) * len);
-
-    for (int i = len; i > 0; i--) {
-        tmp[i - 1] = s[len - i];
-    }
-    return tmp;
-}
 
 void age_page(struct VirtualPage* vp) {
     int len = strlen(vp->counter_bits_string);
     char new_bit = vp->r_bit + '0';
-    if (len >= 8) {
-        vp->counter_bits_string[0] = '\0';
-        vp->counter_bits_string++;
-    }
 
-    vp->counter_bits_string[len] = new_bit;
-    char* reversed = revstr(vp->counter_bits_string);
-    vp->counter = (int) strtol(reversed, NULL, 2);
+//    int skip_shift_for_last_elem = (len >= 8);
+    for (int i = len - 1; i >= 1; --i) {
+        vp->counter_bits_string[i] = vp->counter_bits_string[i - 1];
+    }
+    vp->counter_bits_string[0] = new_bit;
+//    vp->counter_bits_string[len] = '\0';
+
+
+    vp->counter = (int) strtol(vp->counter_bits_string, NULL, 2);
 //    if (vp->counter != 0) {
-//        printf("%d\n", vp->counter);
+//        printf("%d %s %d\n", vp->number, vp->counter_bits_string, vp->counter);
 //    }
 }
 
@@ -46,24 +39,21 @@ void func(int page_frames_count) {
 
     for (int i = 0; i < 1024; ++i) {
         t_data *vp;
-        vp = malloc(sizeof(t_data) + sizeof(char) * 1024);
+        vp = malloc(sizeof(t_data));
         vp->number = i;
         vp->counter = 0;
         vp->r_bit = 0;
         vp->is_present = 0;
-        vp->counter_bits_string = malloc(sizeof(char) * 1024);
-        vp->counter_bits_string[0] = '0';
-
-//        struct VirtualPage vp = { i, 0, 0, 0 };
+//        vp->counter_bits_string[0] = '0';
+        for (int j = 0; j < AGING_BITS; ++j) {
+            vp->counter_bits_string[j] = '0';
+        }
         virtual_mem_pages[i] = vp;
     }
 
-    int page_number;
-    int cnt = 0;
+    int n;
     FILE * f = fopen("input.txt", "r");
-    while (fscanf(f, "%d", &page_number) == 1) {
-        int n = page_number;
-//        printf("%d\n", ++cnt);
+    while (fscanf(f, "%d", &n) == 1) {
         int found_flag = 0;
 
         struct VirtualPage* present_pages[1024];
@@ -75,13 +65,14 @@ void func(int page_frames_count) {
             virtual_mem_pages[i]->r_bit = 0;
 
             // Form an array of present in memory pages
-            if (virtual_mem_pages[i]->is_present == 1) {
+            if (virtual_mem_pages[i]->is_present) {
                 present_pages[present_cnt] = virtual_mem_pages[i];
                 present_cnt++;
 
-                // Given memory page is present
+                // Found page on physical memory
                 if (i == n) {
                     hits++;
+                    virtual_mem_pages[n]->r_bit = 1;
                     found_flag = 1;
                 }
             }
@@ -92,7 +83,7 @@ void func(int page_frames_count) {
 
         // If there is not enough memory to load new page
         if (present_cnt >= page_frames_count) {
-            // Select worst page in terms of aging algo
+            // Select worst page to remove in terms of aging algo
             int min_counter_bits = MAX_COUNTER_INT;
             struct VirtualPage* remove_candidate = present_pages[0];
             for (int i = 0; i < present_cnt; ++i) {
