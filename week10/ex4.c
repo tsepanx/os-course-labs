6d6da2702d336687a047194279632cadf238aeeb
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-11-14 16:43:24 +0300

    week10: ex4

--- week10/ex4.c
+++ week10/ex4.c
@@ -15,12 +15,13 @@ char* path(struct dirent* dp, char* dirname) {
 int main() {
     char* dirname = "tmp";
     DIR* dirp = opendir(dirname);
-    if ( dirp == NULL ) { return 1 ; }
+    if (dirp == NULL) { return 1 ; }
 
-    struct dirent **namelist = malloc(sizeof(struct dirent) * 8);
+    struct dirent **namelist = malloc(sizeof(struct dirent) * 16);
     int i = 0;
 
     struct dirent *d;
+    // fill namelist arr with all "dirent" entries in directory
     while ((d = readdir(dirp)) != NULL ) {
         namelist[i] = d;
         i++;
@@ -28,6 +29,7 @@ int main() {
 
     printf("File —– Hard Links\n");
 
+    // iterate over entries
     for (int j = 0; j < i; ++j) {
         struct dirent *dp = namelist[j];
 
@@ -38,10 +40,12 @@ int main() {
             if (buff.st_nlink > 1) {
                 printf("%s - ", path(dp, dirname));
 
+                // Iterate over all entries, to determine some with the same inode number
                 for (int k = 0; k < i; ++k) {
                     struct dirent *dpk = namelist[k];
                     struct stat buff2;
                     stat(path(dpk, dirname), &buff2);
+                    // if inode number is the same, and name is different
                     if (buff.st_ino == buff2.st_ino && strcmp(dp->d_name, dpk->d_name) != 0) {
                         printf("%s,", dpk->d_name);
                     }
